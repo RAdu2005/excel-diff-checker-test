52c67904e49aed952fc8d299a5a2904b5fc40d0d
Radu Stoiana <radu.stoiana@gmail.com>
2024-10-17 13:23:42 +0300

    Unit test class for CallableValue
    Three unit tests for CallableValue.java class

---
+++ src/main/java/edu/abhi/poi/excel/CallableValueTest.java
@@ -0,0 +1,35 @@
+package edu.abhi.poi.excel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class CallableValueTest {
+
+    private CallableValue cv;
+
+    @Before
+    public void setup(){
+        cv = new CallableValue();
+    }
+
+    @Test
+    public void setFailedFalseTest(){
+        cv.setFailed(false);
+        assertFalse(cv.isFailed());
+    }
+
+    @Test
+    public void setFailedTrueTest(){
+        cv.setFailed(true);
+        assertTrue(cv.isFailed());
+    }
+
+    @Test
+    public void setExceptionTest(){
+        Exception e = new RuntimeException("This is an error!");
+        cv.setException(e);
+        assertEquals(e,cv.getException());
+    }
+}
